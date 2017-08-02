c867c2b225fdf8e957c9a00d50c30e5e86c80ebd
nachivpn <nachivpn@gmail.com>
2017-08-02 12:28:01 +0200

    corrects either package name

--- src/main/java/cern/molr/type/either/Either.java
+++ src/main/java/cern/molr/type/either/Either.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2017 European Organisation for Nuclear Research (CERN), All Rights Reserved.
  */
 
-package cern.molr.type;
+package cern.molr.type.either;
 
 import java.util.function.Function;
 /**
